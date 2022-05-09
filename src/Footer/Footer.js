b470dab2cf598f05241b072aaef2a240b26672a9
Eleonora Di Pierro <eleonora.dipierro@gmail.com>
2022-05-09 23:57:47 +0200

    extract MobileFooter and DesktopFooter

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -24,7 +24,157 @@ const MenuItem = (props) => {
   );
 };
 
+const MobileFooter = (props) => {
+  const { allSubmenu } = props;
+  return (
+    <div className="footerMenuMobile">
+      <ul className="footerMenuMobile__items footerMenuMobile__items--mobile">
+        {allSubmenu.map((menu, i) => {
+          return (
+            <MenuItem title={menu.name}>
+              <ul className="itemsMobile__detail">
+                {menu.submenu.map((current, j) => {
+                  return (
+                    <li>
+                      <a href="">{current}</a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </MenuItem>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const DesktopFooter = (props) => {
+  const { allSubmenu } = props;
+  return (
+    <div className="footerMenuDesktop">
+      {allSubmenu.map((menu, i) => {
+        return (
+          <div className="footerMenuDesktop__items">
+            <ul className="footerMenuDesktop__list">
+              <li className="footerMenuDesktop__list__boldElem">{menu.name}</li>
+              {menu.submenu.map((current, j) => {
+                return (
+                  <li>
+                    <a href="">{current}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 const Footer = () => {
+  const allSubmenu = [
+    {
+      name: "Shop and Learn",
+      submenu: [
+        "Store",
+        "Mac",
+        "iPad",
+        "iPhone",
+        "Watch",
+        "TV & Home",
+        "iPod touch",
+        "AirTag",
+        "Accessories",
+        "Gift Cards",
+      ],
+    },
+    {
+      name: "Services",
+      submenu: [
+        "Apple Music",
+        "Apple TV +",
+        "Apple Fitness+",
+        "Apple News+",
+        "Apple Arcade",
+        "iCloud",
+        "Apple One",
+        "Apple Pay",
+        "Apple Books",
+        "Apple Podcasts",
+        "App Store",
+      ],
+    },
+    {
+      name: "Account",
+      submenu: ["Manage Your Apple ID", "Apple Store Account", "iCloud.com"],
+    },
+    {
+      name: "Apple Store",
+      submenu: [
+        "Find a Store",
+        "Genius Bar",
+        "Today at Apple",
+        "Apple Summer Camp",
+        "Apple Store App",
+        "Refurbished and Clearance",
+        "Financing",
+        "Apple Trade In",
+        "Order Status",
+        "Shopping Help",
+      ],
+    },
+    {
+      name: "For Business",
+      submenu: ["Apple and Business", "Shop for Business"],
+    },
+    {
+      name: "For Education",
+      submenu: ["Apple and Education", "Shop for University"],
+    },
+    {
+      name: "For Healthcare",
+      submenu: [
+        "Apple in Healthcare",
+        "Health on Apple Watch",
+        "Health Records on iPhone",
+      ],
+    },
+    {
+      name: "Apple Values",
+      submenu: [
+        "Accessibility",
+        "Environment",
+        "Privacy",
+        "Supplier Responsibility",
+      ],
+    },
+    {
+      name: "About Apple",
+      submenu: [
+        "Newsroom",
+        "Apple Leadership",
+        "Career Opportunities",
+        "Warranty",
+        "Investors",
+        "Ethics & Compliance",
+        "Events",
+        "European Job Creation",
+        "Contact Apple",
+      ],
+    },
+  ];
+
+  const creditsList = [
+    "Privacy Policy",
+    "Use of Cookies",
+    "Terms of Use",
+    "Sales and Refunds",
+    "Legal",
+    "Site Map",
+  ];
+
   return (
     <footer>
       <div className="wrapper">
@@ -36,432 +186,8 @@ const Footer = () => {
             Restrictions and other <a href="">terms</a> apply.
           </div>
 
-          <div className="footerMenuMobile">
-            <ul className="footerMenuMobile__items footerMenuMobile__items--mobile">
-              <MenuItem title="Shop and Learn">
-                <ul className="itemsMobile__detail">
-                  <li>
-                    <a href="">Store</a>
-                  </li>
-                  <li>
-                    <a href="">Mac</a>
-                  </li>
-                  <li>
-                    <a href="">iPad</a>
-                  </li>
-                  <li>
-                    <a href="">iPhone</a>
-                  </li>
-                  <li>
-                    <a href="">Watch</a>
-                  </li>
-                  <li>
-                    <a href="">TV & Home</a>
-                  </li>
-                  <li>
-                    <a href="">iPod touch</a>
-                  </li>
-                  <li>
-                    <a href="">AirTag</a>
-                  </li>
-                  <li>
-                    <a href="">Accessories</a>
-                  </li>
-                  <li>
-                    <a href="">Gift Cards</a>
-                  </li>
-                </ul>
-              </MenuItem>
-              <MenuItem title="Services">
-                <ul className="items__detail">
-                  <li>
-                    <a href="">Apple Music</a>
-                  </li>
-                  <li>
-                    <a href="">Apple TV +</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Fitness+</a>
-                  </li>
-                  <li>
-                    <a href="">Apple News+</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Arcade</a>
-                  </li>
-                  <li>
-                    <a href="">iCloud</a>
-                  </li>
-                  <li>
-                    <a href="">Apple One</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Pay</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Books</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Podcasts</a>
-                  </li>
-                  <li>
-                    <a href="">App Store</a>
-                  </li>
-                </ul>
-              </MenuItem>
-              <MenuItem title="Account">
-                <ul className="items__detail">
-                  <li>
-                    <a href="">Manage Your Apple ID</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Store Account</a>
-                  </li>
-                  <li>
-                    <a href="">iCloud.com</a>
-                  </li>
-                </ul>
-              </MenuItem>
-              <MenuItem title="Apple Store">
-                <ul className="items__detail">
-                  <li>
-                    <a href="">Find a Store</a>
-                  </li>
-                  <li>
-                    <a href="">Genius Bar</a>
-                  </li>
-                  <li>
-                    <a href="">Today at Apple</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Summer Camp</a>
-                  </li>
-                  <li>
-                    <a href=""> Apple Store App</a>
-                  </li>
-                  <li>
-                    <a href="">Refurbished and Clearance</a>
-                  </li>
-                  <li>
-                    <a href="">Financing</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Trade In</a>
-                  </li>
-                  <li>
-                    <a href="">Order Status</a>
-                  </li>
-                  <li>
-                    <a href="">Shopping Help</a>
-                  </li>
-                </ul>
-              </MenuItem>
-              <MenuItem title="For Business">
-                <ul className="items__detail">
-                  <li>
-                    <a href="">Apple and Business</a>
-                  </li>
-                  <li>
-                    <a href="">Shop for Business</a>
-                  </li>
-                </ul>
-              </MenuItem>
-              <MenuItem title="For Education">
-                <ul className="items__detail">
-                  <li>
-                    <a href="">Apple and Education</a>
-                  </li>
-                  <li>
-                    <a href="">Shop for University</a>
-                  </li>
-                </ul>
-              </MenuItem>
-              <MenuItem title="For Healthcare">
-                <ul className="items__detail">
-                  <li>
-                    <a href="">Apple in Healthcare</a>
-                  </li>
-                  <li>
-                    <a href="">Health on Apple Watch</a>
-                  </li>
-                  <li>
-                    <a href="">Health Records on iPhone</a>
-                  </li>
-                </ul>
-              </MenuItem>
-              <MenuItem title=" Apple Values">
-                <ul className="items__detail">
-                  <li>
-                    <a href="">Accessibility</a>
-                  </li>
-                  <li>
-                    <a href="">Environment</a>
-                  </li>
-                  <li>
-                    <a href="">Privacy</a>
-                  </li>
-                  <li>
-                    <a href="">Supplier Responsibility</a>
-                  </li>
-                </ul>
-              </MenuItem>
-              <MenuItem title="About Apple">
-                <ul className="items__detail">
-                  <li>
-                    <a href="">Newsroom</a>
-                  </li>
-                  <li>
-                    <a href="">Apple Leadership</a>
-                  </li>
-                  <li>
-                    <a href="">Career Opportunities</a>
-                  </li>
-                  <li>
-                    <a href="">Warranty</a>
-                  </li>
-                  <li>
-                    <a href="">Investors</a>
-                  </li>
-                  <li>
-                    <a href="">Ethics & Compliance</a>
-                  </li>
-                  <li>
-                    <a href="">Events</a>
-                  </li>
-                  <li>
-                    <a href="">European Job Creation</a>
-                  </li>
-                  <li>
-                    <a href="">Contact Apple</a>
-                  </li>
-                </ul>
-              </MenuItem>
-            </ul>
-          </div>
-
-          <div className="footerMenuDesktop">
-            <div className="footerMenuDesktop__items">
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">
-                  Shop and Learn
-                </li>
-                <li>
-                  <a href="">Store</a>
-                </li>
-                <li>
-                  <a href="">Mac</a>
-                </li>
-                <li>
-                  <a href="">iPad</a>
-                </li>
-                <li>
-                  <a href="">iPhone</a>
-                </li>
-                <li>
-                  <a href="">Watch</a>
-                </li>
-                <li>
-                  <a href="">TV & Home</a>
-                </li>
-                <li>
-                  <a href="">iPod touch</a>
-                </li>
-                <li>
-                  <a href="">AirTag</a>
-                </li>
-                <li>
-                  <a href="">Accessories</a>
-                </li>
-                <li>
-                  <a href="">Gift Cards</a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="footerMenuDesktop__items">
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">Services</li>
-                <li>
-                  <a href="">Apple Music</a>
-                </li>
-                <li>
-                  <a href="">Apple TV +</a>
-                </li>
-                <li>
-                  <a href="">Apple Fitness+</a>
-                </li>
-                <li>
-                  <a href="">Apple News+</a>
-                </li>
-                <li>
-                  <a href="">Apple Arcade</a>
-                </li>
-                <li>
-                  <a href="">iCloud</a>
-                </li>
-                <li>
-                  <a href="">Apple One</a>
-                </li>
-                <li>
-                  <a href="">Apple Pay</a>
-                </li>
-                <li>
-                  <a href="">Apple Books</a>
-                </li>
-                <li>
-                  <a href="">Apple Podcasts</a>
-                </li>
-                <li>
-                  <a href="">App Store</a>
-                </li>
-              </ul>
-
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">Account</li>
-                <li>
-                  <a href="">Manage Your Apple ID</a>
-                </li>
-                <li>
-                  <a href="">Apple Store Account</a>
-                </li>
-                <li>
-                  <a href="">iCloud.com</a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="footerMenuDesktop__items">
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">
-                  Apple Store
-                </li>
-                <li>
-                  <a href="">Find a Store</a>
-                </li>
-                <li>
-                  <a href="">Genius Bar</a>
-                </li>
-                <li>
-                  <a href="">Today at Apple</a>
-                </li>
-                <li>
-                  <a href="">Apple Summer Camp</a>
-                </li>
-                <li>
-                  <a href=""> Apple Store App</a>
-                </li>
-                <li>
-                  <a href="">Refurbished and Clearance</a>
-                </li>
-                <li>
-                  <a href="">Financing</a>
-                </li>
-                <li>
-                  <a href="">Apple Trade In</a>
-                </li>
-                <li>
-                  <a href="">Order Status</a>
-                </li>
-                <li>
-                  <a href="">Shopping Help</a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="footerMenuDesktop__items">
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">
-                  For Business
-                </li>
-                <li>
-                  <a href="">Apple and Business</a>
-                </li>
-                <li>
-                  <a href="">Shop for Business</a>
-                </li>
-              </ul>
-
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">
-                  For Education
-                </li>
-                <li>
-                  <a href="">Apple and Education</a>
-                </li>
-                <li>
-                  <a href="">Shop for University</a>
-                </li>
-              </ul>
-
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">
-                  For Healthcare
-                </li>
-                <li>
-                  <a href="">Apple in Healthcare</a>
-                </li>
-                <li>
-                  <a href="">Health on Apple Watch</a>
-                </li>
-                <li>
-                  <a href="">Health Records on iPhone</a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="footerMenuDesktop__items">
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">
-                  Apple Values
-                </li>
-                <li>
-                  <a href="">Accessibility</a>
-                </li>
-                <li>
-                  <a href="">Environment</a>
-                </li>
-                <li>
-                  <a href="">Privacy</a>
-                </li>
-                <li>
-                  <a href="">Supplier Responsibility</a>
-                </li>
-              </ul>
-
-              <ul className="footerMenuDesktop__list">
-                <li className="footerMenuDesktop__list__boldElem">
-                  About People
-                </li>
-                <li>
-                  <a href="">Newsroom</a>
-                </li>
-                <li>
-                  <a href="">Apple Leadership</a>
-                </li>
-                <li>
-                  <a href="">Career Opportunities</a>
-                </li>
-                <li>
-                  <a href="">Warranty</a>
-                </li>
-                <li>
-                  <a href="">Investors</a>
-                </li>
-                <li>
-                  <a href="">Ethics & Compliance</a>
-                </li>
-                <li>
-                  <a href="">Events</a>
-                </li>
-                <li>
-                  <a href="">European Job Creation</a>
-                </li>
-                <li>
-                  <a href="">Contact Apple</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <MobileFooter allSubmenu={allSubmenu} />
+          <DesktopFooter allSubmenu={allSubmenu} />
         </div>
 
         <div>
@@ -478,35 +204,22 @@ const Footer = () => {
           </div>
         </div>
 
-        <div>
-          <div className="footerCredits">
-            <div className="footerCredits__items">
-              <div>Copyright © 2022 Apple Inc. All rights reserved.</div>
+        <div className="footerCredits">
+          <div className="footerCredits__items">
+            <div>Copyright © 2022 Apple Inc. All rights reserved.</div>
 
-              <div className="footerCredits__order">
-                <div className="footerCredits__order__details">
-                  <a href=""> Privacy Policy </a>
-                </div>
-                <div className="footerCredits__order__details">
-                  <a href="">Use of Cookies</a>
-                </div>
-                <div className="footerCredits__order__details">
-                  <a href="">Terms of Use</a>
-                </div>
-                <div className="footerCredits__order__details">
-                  <a href="">Sales and Refunds</a>
-                </div>
-                <div className="footerCredits__order__details">
-                  <a href="">Legal</a>
-                </div>
-                <div className="footerCredits__order__details">
-                  <a href="">Site Map</a>
-                </div>
-              </div>
+            <div className="footerCredits__order">
+              {creditsList.map((item, i) => {
+                return (
+                  <div className="footerCredits__order__details">
+                    <a href=""> {item} </a>
+                  </div>
+                );
+              })}
+            </div>
 
-              <div className="footerCredits__order__languageAlign">
-                <a href="">United Kingdom</a>
-              </div>
+            <div className="footerCredits__order__languageAlign">
+              <a href="">United Kingdom</a>
             </div>
           </div>
         </div>
